193bd1ff9ecc9ef6acf8306c462e6000d886cc0e
linus <linusedstrom80@gmail.com>
2025-11-30 15:22:21 +0100

    sundayLastFix

--- Main.java
+++ Main.java
@@ -11,11 +11,12 @@ import javafx.animation.Timeline;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.awt.*;
-import java.net.URL;
+
 
 public class Main extends Application {
 
@@ -30,16 +31,18 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         stage.setTitle(" MemberRegister and Timer");
 
     GridPane grid = new GridPane();
-    grid.setPadding(new Insets(40, 30, 40, 30));
-    grid.setHgap(14);
+    //grid.setGridLinesVisible(true);
+    grid.setPadding(new Insets(15, 10, 15, 10));
+    grid.setHgap(10);
     grid.setVgap(10);
 
     // First name
     Label firstName = new Label("Enter first name");
+    firstName.setPrefWidth(450);
     GridPane.setConstraints(firstName, 0, 0);
 
     TextField firstNameInput = new TextField();
@@ -47,33 +50,39 @@ public class Main extends Application {
 
     // Last name
     Label lastName = new Label(" Enter last name");
-    GridPane.setConstraints(lastName, 0, 5);
+    GridPane.setConstraints(lastName, 0, 2);
 
     TextField lastNameInput = new TextField();
-    GridPane.setConstraints(lastNameInput, 0, 6);
+    GridPane.setConstraints(lastNameInput, 0, 3);
 
     //Phone number
     Label phoneNumber = new Label("Enter phone number");
-    GridPane.setConstraints(phoneNumber, 0, 10);
+    GridPane.setConstraints(phoneNumber, 0, 4);
 
     TextField phoneNumberInput = new TextField();
-    GridPane.setConstraints(phoneNumberInput, 0, 11);
+    GridPane.setConstraints(phoneNumberInput, 0, 5);
 
     //Adress
     Label address = new Label("Enter address");
-    GridPane.setConstraints(address, 0,15);
+    GridPane.setConstraints(address, 0,6);
 
     TextField addressInput = new TextField();
-    GridPane.setConstraints(addressInput, 0, 16);
+    GridPane.setConstraints(addressInput, 0, 7);
 
     Label newMember = new Label();
-    newMember.setStyle("-fx-font-size: 15;");
-    GridPane.setConstraints(newMember, 0, 23);
+    newMember.setStyle("-fx-font-size: 12;");
+    GridPane.setConstraints(newMember, 0, 9);
 
     Button button = new Button(" Save member");
+         button.setStyle("-fx-background-radius: 0;" +
+                "-fx-background-color: lightgray;" +
+                "-fx-font-size: 12px;" +
+                "-fx-pref-width: 120px;" +
+                "-fx-max-heigth: 30px;"
 
+        );
 
-    GridPane.setConstraints(button, 0, 21);
+    GridPane.setConstraints(button, 0, 8);
     button.setOnAction(saveMember -> {
                 newMember.setText("New member saved: " + firstNameInput.getText() + " ," +
                         lastNameInput.getText() + " ," + phoneNumberInput.getText() +
@@ -82,16 +91,13 @@ public class Main extends Application {
 
     Button start = new Button("START");
     start.setStyle("-fx-background-color: green;");
-    GridPane.setConstraints(start, 12, 1);
     start.setOnAction(time -> timerTime.play());
 
     Button stop = new Button("STOP");
     stop.setStyle("-fx-background-color: red;");
-    GridPane.setConstraints(stop, 12, 5);
     stop.setOnAction(time -> timerTime.stop());
 
     Label timerLabel = new Label("00:00:00");
-    GridPane.setConstraints(timerLabel, 12, 9);
 
     timerTime = new Timeline (new KeyFrame(Duration.seconds(1), timer -> {
         seconds++;
@@ -109,31 +115,21 @@ public class Main extends Application {
         }));
         timerTime.setCycleCount(Timeline.INDEFINITE); //OCKSÅ EN FETING KONSTANT HÄMTAD FRÅN NÅN GOLISTA
 
-
-
-
-
+        VBox timerArea = new VBox();
+        timerArea.setSpacing(10);
+        GridPane.setConstraints(timerArea, 16, 8);
+        timerArea.getChildren().addAll(start, stop, timerLabel);
 
 
         grid.getChildren().addAll(firstName, firstNameInput, lastName, lastNameInput,
-                phoneNumber, phoneNumberInput, address, addressInput, newMember, button, timerLabel, start, stop);
+                phoneNumber, phoneNumberInput, address, addressInput, newMember, button, timerArea);
 
         //Testar lägga in ett styles.css, ett CSS stylesheet
 
-
-        Scene scene = new Scene(grid, 600, 1000);
+        Scene scene = new Scene(grid, 800, 600);
         scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
         // getClass().getResource returnerar en URL. /styles är för att man letar styles från root katalogen scr/main/resources där den brukar ligga
         // getResource("/styles.css").toExternalForm());
-        Platform.runLater(()-> { button.setStyle("-fx-background-radius: 0;" + // Gör en Platform.runLater(() för att
-                // det ska garantera att den här knappens button.setStyle inte blir överkörd av den i styles.css
-                "-fx-background-color: lightgray;" +
-                "-fx-font-size: 12px;" +
-                "-fx-pref-width: 100px;" +
-                "-fx-pre-heigth: 60px;"
-        );
-        });
-
 
         stage.setScene(scene);
         stage.show();
@@ -150,12 +146,6 @@ public class Main extends Application {
 
 
 
-/*
-    private void stoptCounting(){
-    if(worker != null && worker.isAlive()){
-        worker.interrupt();
-    }
 
- */
 
 
